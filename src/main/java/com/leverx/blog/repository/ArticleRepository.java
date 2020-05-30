9014d880038f6ed506aa88d9564c72b887dad274
victoria <vika.zhak1999@gmail.com>
2020-05-31 02:44:12 +0300

    implement methods

--- src/main/java/com/leverx/blog/repository/ArticleRepository.java
+++ src/main/java/com/leverx/blog/repository/ArticleRepository.java
@@ -1,15 +1,20 @@
 package com.leverx.blog.repository;
 
 import com.leverx.blog.model.Article;
-import com.leverx.blog.model.dto.ArticleDto;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ArticleRepository {
-    Optional<Article>findById(Integer id);
-    Article create(Article article);
+    Optional<Article> findById(Integer id);
+
+    Integer create(Article article);
+
     Article update(Article article);
+
     void delete(Integer id);
-    List<ArticleDto> findAll();
+
+    List<Article> findAll();
+
+    Optional<Article> findByName(String name);
 }
--- src/main/java/com/leverx/blog/repository/impl/ArticleRepositoryImpl.java
+++ src/main/java/com/leverx/blog/repository/impl/ArticleRepositoryImpl.java
@@ -1,34 +1,40 @@
 package com.leverx.blog.repository.impl;
 
 import com.leverx.blog.model.Article;
-import com.leverx.blog.model.dto.ArticleDto;
 import com.leverx.blog.repository.ArticleRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import java.util.List;
 import java.util.Optional;
 
-public class ArticleRepositoryImpl implements ArticleRepository {
-    private final Class<Article> entityClass;
 
+@Repository
+public class ArticleRepositoryImpl implements ArticleRepository {
+
+    public static final String SELECT_A_TITLE_FROM_ARTICLE_A_WHERE_A_TITLE_TITLE = "SELECT a.title from Article a where a.title = :title";
+    public static final String SELECT_ID_TITLE_TEXT_STATUS_AUTHOR_ID_UPDATED_AT_CREATED_AT_FROM_ARTICLE = "SELECT id, title, text, status, author_id, updated_at, created_at from Article";
     @PersistenceContext
     EntityManager entityManager;
 
-    ArticleRepositoryImpl(Class<Article> entityClass) {
-        this.entityClass = entityClass;
+    public ArticleRepositoryImpl() {
     }
+
 
     @Override
     public Optional<Article> findById(Integer id) {
-        return Optional.ofNullable(entityManager.find(entityClass, id));
+        return Optional.ofNullable(entityManager.find(Article.class, id));
     }
 
-    //bad
+
     @Override
-    public Article create(Article article) {
+    public Integer create(Article article) {
         entityManager.persist(article);
-        return article;
+        return article.getId();
     }
 
     @Override
@@ -38,12 +44,24 @@ public class ArticleRepositoryImpl implements ArticleRepository {
 
     @Override
     public void delete(Integer id) {
-        Article reference = entityManager.getReference(entityClass,id);
+        Article reference = entityManager.getReference(Article.class, id);
         entityManager.remove(reference);
     }
 
+    /*not working*/
     @Override
-    public List<ArticleDto> findAll() {
-        return null;
+    public List<Article> findAll() {
+        Query query = entityManager.createQuery(SELECT_ID_TITLE_TEXT_STATUS_AUTHOR_ID_UPDATED_AT_CREATED_AT_FROM_ARTICLE);
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Article> criteriaQuery = criteriaBuilder.createQuery(Article.class);
+        return entityManager.createQuery(criteriaQuery)
+                .getResultList();
+    }
+
+    @Override
+    public Optional<Article> findByName(String name) {
+        Query query = entityManager.createQuery(SELECT_A_TITLE_FROM_ARTICLE_A_WHERE_A_TITLE_TITLE);
+        query.setParameter("title", name);
+        return (Optional.ofNullable((Article) query.getSingleResult()));
     }
 }
