d91acaf3a9a3afbdaee8ac77e3940e5d07cfb7d9
victoria <vika.zhak1999@gmail.com>
2020-05-31 02:44:42 +0300

    add exception for duplicating names

---
+++ src/main/java/com/leverx/blog/exception/NameAlreadyExistException.java
@@ -0,0 +1,23 @@
+package com.leverx.blog.exception;
+
+public class NameAlreadyExistException extends RuntimeException {
+    public NameAlreadyExistException() {
+        super();
+    }
+
+    public NameAlreadyExistException(String message) {
+        super(message);
+    }
+
+    public NameAlreadyExistException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NameAlreadyExistException(Throwable cause) {
+        super(cause);
+    }
+
+    protected NameAlreadyExistException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
